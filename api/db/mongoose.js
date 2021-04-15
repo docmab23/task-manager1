d388da35a0bed5dec18b1e55ff44a468fe42f897
Vinay Gupta <sunriseappsinc@gmail.com>
2021-04-15 14:12:28 +0530

    added config mongo

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TaskManager', { useNewUrlParser: true }).then(() => {
+mongoose.connect('mongodb://Vinay:8lU6laMCHUx27twW@cluster0-shard-00-00.otxsm.mongodb.net:27017,cluster0-shard-00-01.otxsm.mongodb.net:27017,cluster0-shard-00-02.otxsm.mongodb.net:27017/task?ssl=true&replicaSet=atlas-vdctk7-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => {
     console.log("Connected to MongoDB successfully :)");
 }).catch((e) => {
     console.log("Error while attempting to connect to MongoDB");
@@ -18,3 +18,5 @@ mongoose.set('useFindAndModify', false);
 module.exports = {
     mongoose
 };
+
+// 8lU6laMCHUx27twW
